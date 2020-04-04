595cd0ecab4cfcafec8f0fdf9b441ae87370fb13
Dev-veD <divyansh.code@gmail.com>
2020-04-04 13:55:19 +0530

    Comyt

---
+++ CoolAdmin-master/index.php
@@ -0,0 +1,21 @@
+<?php include "headn.html";?>
+<?php include "headerD.html";?>
+<?php include "headerM.html";?>
+
+
+<body class="animsition">
+    <div class="page-wrapper">
+       
+        
+<div class='text-center'><h1>Uttarakhand COVID19</h1></div>
+        <!-- PAGE CONTENT-->
+        <div class="page-content--bgf7">
+            <!-- STATISTIC-->
+            <h1>Location <br></h1><br>
+<div class="mapouter"><div class="gmap_canvas"><iframe width="1200" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=uttarakhand&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net">Google Maps</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
+
+
+            <!-- END STATISTIC-->
+            <?php include "tableM.html";?>
+
+            <?php include "footm.html";?>
